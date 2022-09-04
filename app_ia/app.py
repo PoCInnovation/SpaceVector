de51346de18d0f9293b0221441769d58100f8d2d
LayBraid <clement.loeuillet@hotmail.com>
2022-09-04 18:34:45 +0200

    Backend ok + docker compose ok

--- app_ia/app.py
+++ app_ia/app.py
@@ -1,5 +1,5 @@
 import os
-from typing import Union
+import sys
 
 import numpy as np
 from PIL import Image
@@ -10,79 +10,74 @@ from fastapi import FastAPI
 
 backend = FastAPI()
 
+connections.connect("default", host="localhost", port="19530")
 
-def connect_db():
-    connections.connect("default", host="localhost", port="19530")
+collection_name = "SpaceVectorData"
+dim = 512
+default_fields = [
+    FieldSchema(name="id", dtype=DataType.INT64, is_primary=True, auto_id=True),
+    FieldSchema(name="vector", dtype=DataType.FLOAT_VECTOR, dim=dim)
+]
+default_schema = CollectionSchema(fields=default_fields, description="Image data")
 
+collection = Collection(name=collection_name, schema=default_schema)
 
-class App:
-    def __init__(self):
-        connect_db()
-        self.collection_name = "SpaceVectorData"
-        self.dim = 512
-        self.default_fields = [
-            FieldSchema(name="id", dtype=DataType.INT64, is_primary=True, auto_id=True),
-            FieldSchema(name="vector", dtype=DataType.FLOAT_VECTOR, dim=self.dim),
-            FieldSchema(name="path", dtype=DataType.STRING)
-        ]
-        self.default_schema = CollectionSchema(fields=self.default_fields, description="Image data")
-
-        self.collection = Collection(name=self.collection_name, schema=self.default_schema)
-
-        self.default_index = {"index_type": "IVF_SQ8", "params": {"nlist": 512}, "metric_type": "L2"}
-        self.collection.create_index(field_name="vector", index_params=self.default_index)
-        self.collection.load()
-
-        self.data_dir = "./data/images/"
-
-        self.model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
-        self.processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
-
-    def upload_data(self):
-        for filename in tqdm(os.listdir(self.data_dir)):
-            if filename.endswith(".jpg"):
-                img = Image.open(os.path.join(self.data_dir, filename))
-                input_ids = self.processor(text=None, images=img, return_tensors="pt", padding=True)
-                img = self.model.get_image_features(**input_ids)
-                img = np.array(img.detach())
-                img = img.reshape(1, -1)
-                mr = self.collection.insert([img, filename])
+default_index = {"index_type": "IVF_SQ8", "params": {"nlist": 512}, "metric_type": "L2"}
+collection.create_index(field_name="vector", index_params=default_index)
+collection.load()
 
 
-print(f"Prepare App...")
-app = App()
+def upload_data():
+    data_dir = "./data/images/"
+    for filename in tqdm(os.listdir(data_dir)):
+        if filename.endswith(".jpg"):
+            img = Image.open(os.path.join(data_dir, filename))
+            input_ids = processor(text=None, images=img, return_tensors="pt", padding=True)
+            img = model.get_image_features(**input_ids)
+            img = np.array(img.detach())
+            img = img.reshape(1, -1)
+            rm = collection.insert([img])
+            print(rm)
 
-# app.upload_data()
+
+model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == "upload_data":
+        upload_data()
 
 search_params = {"metric_type": "L2", "params": {"nprobe": 10}}
 
 
-@backend.get("/query/{text}")
-async def read_user_item(text: str, q: Union[str] = None, short: bool = False):
-    item = {"text": str}
+@backend.get("/query/")
+async def read_user_item():
+    params = "pool"
 
-    inputs = app.processor(text=[item], images=None, return_tensors="pt", padding=True)
+    print(f"Query: {params}")
 
-    vector = app.model.get_text_features(**inputs)
+    inputs = processor(text=params, images=None, return_tensors="pt", padding=True)
+
+    vector = model.get_text_features(**inputs)
     vector = np.array(vector.detach())
     vector = vector.reshape(1, -1)
 
     print("Searching...")
-    results = app.collection.search(data=vector,
-                                    anns_field="vector",
-                                    param=search_params,
-                                    limit=1)
+    results = collection.search(data=vector,
+                                anns_field="vector",
+                                param=search_params,
+                                limit=1)
 
     hits = results[0]
     print(f"\n\n\n")
     print(f"- Total hits: {len(hits)}, hits ids: {hits.ids} ")
     print(f"- Top1 hit id: {hits[0].id}, distance: {hits[0].distance}, score: {hits[0].score} ")
 
-    query_result = app.collection.query(expr="id ==" + str(hits[0].id),
-                                        output_fields=["vector", "path"],
-                                        consistency_level="Strong")
+    query_result = collection.query(expr="id ==" + str(hits[0].id),
+                                    output_fields=["vector"],
+                                    consistency_level="Strong")
     print(f"\n\n\n")
     print(f"- Query result:\n{query_result}")
 
-    app.collection.release()
+    collection.release()
     return query_result
--- app_ia/devops.md
+++ app_ia/devops.md
@@ -8,5 +8,5 @@ docker build -t spacevector-back:dev .
 run container
 
 ```
-docker run -it  --rm -v ${PWD}:/app -p 8080:8080 spacevector-back:dev
+docker run -it  --rm -v ${PWD}:/app -p 8080:1212 spacevector-back:dev
 ```
